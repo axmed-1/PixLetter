c09d175ba7b672e4a7265c00072c149f9dee37b8
axmed <mikailov_akhmed@bk.ru>
2025-11-15 15:58:10 +0300

    12

--- app/src/main/java/com/mikailov/pixletter/MainActivity.kt
+++ app/src/main/java/com/mikailov/pixletter/MainActivity.kt
@@ -5,76 +5,78 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mikailov.pixletter.ui.theme.PixLetterTheme
-import androidx.compose.foundation.layout.Column
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
-import androidx.compose.ui.text.style.TextAlign
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PixLetterTheme {GreetingText(message = "Royal Decree! \n" +
-                    "Dear $@Akhmed, \n" +
-                    "His Majesty the King \n" +
-                    "Invites you to a great feast! \n" +
-                    "Come to the castle before sunset, \n" +
-                    "otherwise, you risk losing your head!", from = "From king Arthur" )
-
-
-                }
+            PixLetterTheme {
+                GreetingText(
+                    message = "Royal Decree! \n" +
+                            "Dear \$@Akhmed, \n" +
+                            "His Majesty the King \n" +
+                            "Invites you to a great feast! \n" +
+                            "Come to the castle before sunset, \n" +
+                            "otherwise, you risk losing your head!",
+                    from = "From king Arthur"
+                )
             }
         }
     }
-
-
-
-
+}
 
 @Preview(showBackground = true)
 @Composable
 fun RoyalInvitationPreview() {
     PixLetterTheme {
-        GreetingText(message = "Royal Decree! \n" +
-                "Dear $@Akhmed, \n" +
-                "His Majesty the King \n" +
-                "Invites you to a great feast! \n" +
-                "Come to the castle before sunset, \n" +
-                "otherwise, you risk losing your head!", from = "From king Arthur", modifier = Modifier.padding(all = 8.dp) )
-
-    }
-}
-@Composable
-fun GreetingText(message: String,from:String,modifier: Modifier = Modifier){
-    Column(verticalArrangement = Arrangement.Center, modifier = modifier.padding(8.dp)) {
-
-        Text(
-            text = message, fontSize = 20.sp,
-            lineHeight = 40.sp, textAlign = TextAlign.Center
+        GreetingText(
+            message = "Royal Decree! \n" +
+                    "Dear \$@Akhmed, \n" +
+                    "His Majesty the King \n" +
+                    "Invites you to a great feast! \n" +
+                    "Come to the castle before sunset, \n" +
+                    "otherwise, you risk losing your head!",
+            from = "From king Arthur",
+            modifier = Modifier.padding(all = 8.dp)
         )
     }
-    Text(
-        text = from,
-                fontSize = 24.sp,modifier = Modifier.padding(all = 16.dp).align(alignment = Alignment.End)
-
-
-    )
-
-
 }
 
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(8.dp)
+    ) {
+        Text(
+            text = message,
+            fontSize = 20.sp,
+            lineHeight = 40.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(all = 16.dp)
+                .align(Alignment.End)
+        )
+    }
+}
